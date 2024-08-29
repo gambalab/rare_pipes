b83ea9291885ccd618b7af77ce2b5cf9650921c0
gambalab <gambalab.bioinformatics@gmail.com>
2024-08-29 16:40:19 +0200

    be sure order of cols is always the same

--- scripts/postprocess_results.R
+++ scripts/postprocess_results.R
@@ -45,8 +45,8 @@ df.vcf.tsv$OMIM.Phenotype = df.omim$Phenotype[match(df.vcf.tsv$Symbol,df.omim$Sy
 df.vcf.tsv = df.vcf.tsv[order(df.vcf.tsv$Maverick.Score,decreasing = T),]
 
 # Order coloumns
-cols = c("CHROM","POS","REF","ALT","QUAL","VarType","Symbol","AD","DP","GQ","GT","VAF","AlphaMissense","ESM1b","EVE","REVEL","Maverick.Score","Orphanet.id","Orphanet.Phenotype","Orphanet.inheritance","OMIM.id","OMIM.title","OMIM.Phenotype")
-df.vcf.tsv = cbind(df.vcf.tsv[,cols],df.vcf.tsv[,!colnames(df.vcf.tsv)%in%cols])
+cols = c("CHROM","POS","REF","ALT","QUAL","VarType","Symbol","AD","DP","MED_DP","MIN_DP","GQ","GT","PL","VAF","Maverick.Score","AlphaMissense","ESM1b","EVE","REVEL","Orphanet.id","Orphanet.Phenotype","Orphanet.inheritance","OMIM.id","OMIM.title","OMIM.Phenotype")
+df.vcf.tsv = cbind(df.vcf.tsv[,colnames(df.rare)%in%cols],df.vcf.tsv[,!colnames(df.vcf.tsv)%in%cols])
 
 # save the results
 write.table(df.vcf.tsv,file = out.res.path,quote = F,sep = "\t",row.names = F)
